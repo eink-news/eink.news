c0ff01d4b88a02e578c5c0630ba7c2c3ccb92488
hacalox <hanadonc@gmail.com>
2017-04-27 11:27:49 +0200

    Fix script Parse

--- src/scripts/tryParser.js
+++ src/scripts/tryParser.js
@@ -3,7 +3,7 @@ import createEbook from '../news-parser/index'
 const parser = process.env.PARSER
 console.log("Parsing ", parser);
 if (parser) {
-  createEbook(parser)
+  createEbook([parser])
 } else {
   console.log("You must define the env variable PARSER to the name of the parser you want to try!")
 }
