64d98d1fe0fe05ab66c3def730aa6844f2be9ecb
Dani carmona serrat <danielcarmona55@gmail.com>
2017-05-18 19:01:45 +0200

    Add healthcheckUrl to schedules.js to do different checks for each source

--- src/crons/schedules.js
+++ src/crons/schedules.js
@@ -21,17 +21,20 @@ const schedules = [
   {
     source: 'askhackernews',
     hourFreq: '*',
-    timeZone: 'America/Los_Angeles'
+    timeZone: 'America/Los_Angeles',
+    healthcheckUrl: 'https://hchk.io/08576787-5063-45ff-ad57-f77338945100'
   },
   {
     source: 'hackernews',
     hourFreq: '*',
-    timeZone: 'America/Los_Angeles'
+    timeZone: 'America/Los_Angeles',
+    healthcheckUrl: 'https://hchk.io/39f91551-ad45-4f81-8c2d-bb1b2bb109b2'
   },
   {
     source: 'indiehackers',
     hourFreq: '*/3',
-    timeZone: 'America/Los_Angeles'
+    timeZone: 'America/Los_Angeles',
+    healthcheckUrl: 'https://hchk.io/8dd2a13f-3300-4dc6-a1c3-84d0f0556097'
   }
 ]
 
