4d70e8110f436e19881b39d9d6b344ba29beba16
hacalox <hanadonc@gmail.com>
2017-06-09 23:30:04 +0200

    Fix HN-comments helper, callback implementation

--- src/helpers/parsing/get-HN-comments.js
+++ src/helpers/parsing/get-HN-comments.js
@@ -19,41 +19,38 @@ const getHNComments = function(articleCommentsHtml){
     // get an array of usernames of each comment
     const usernameRegex = /<span class="comhead">[\s\S]*?<a.*?"hnuser">(.*?)<\/a>/g
     getMatches(articleCommentsHtml, usernameRegex, nComments, 1, (usernames) => {
-
-
-
-    // get an array of comments content
-    const commentsRegex = /<\/span><\/div><br><div class="comment">[\s\S]*?<span class="[\w\d]*">([.\s\S]*?)<span>[\s\S]*?<\/span><div class='reply'>/g
-    const comments = getMatches(articleCommentsHtml, commentsRegex, nComments, 1)
-
-    // get an array of indentations if each comment
-    const commentsIndentationRegex = /<td class='ind'><img src="s\.gif" height="1" width="(\w\d*)"><\/td>/g
-    const commentsIndentation = getMatches(articleCommentsHtml, commentsIndentationRegex, nComments, 1)
-    const newIndentations = commentsIndentation.map((indentation) => parseInt(indentation)/40)
-
-    // variable to store the parsed html of the comments
-    let cleanedArticleContent = ""
-    // if there is one or more comments, add them together and return the final html
-    if (nComments >= 1) {
-      for (var i = 0; i < nComments; i++) {
-        cleanedArticleContent += `<div style="margin-left:${newIndentations[i]}em;"><p><b style="color: grey;">${usernames[i]}: &nbsp;</b>${comments[i]}</p></div>`
-        if (i==nComments-1) {
-          const commentsContent = {
-            nComments: nComments,
-            content: cleanedArticleContent
+      // get an array of comments content
+      const commentsRegex = /<\/span><\/div><br><div class="comment">[\s\S]*?<span class="[\w\d]*">([.\s\S]*?)<span>[\s\S]*?<\/span><div class='reply'>/g
+      getMatches(articleCommentsHtml, commentsRegex, nComments, 1,(comments) => {
+        // get an array of indentations if each comment
+        const commentsIndentationRegex = /<td class='ind'><img src="s\.gif" height="1" width="(\w\d*)"><\/td>/g
+        getMatches(articleCommentsHtml, commentsIndentationRegex, nComments, 1, (commentsIndentation) => {
+          const newIndentations = commentsIndentation.map((indentation) => parseInt(indentation)/40)
+          // variable to store the parsed html of the comments
+          let cleanedArticleContent = ""
+          // if there is one or more comments, add them together and return the final html
+          if (nComments >= 1) {
+            for (var i = 0; i < nComments; i++) {
+              cleanedArticleContent += `<div style="margin-left:${newIndentations[i]}em;"><p><b style="color: grey;">${usernames[i]}: &nbsp;</b>${comments[i]}</p></div>`
+              if (i==nComments-1) {
+                const commentsContent = {
+                  nComments: nComments,
+                  content: cleanedArticleContent
+                }
+                resolve(commentsContent)
+              }
+            }
+          } else {
+            // if there is no comments, tell it
+            cleanedArticleContent += `<div><p><b>This article has no comments yet</b></p></div>`
+            const commentsContent = {
+              nComments: nComments,
+              content: cleanedArticleContent
+            }
+            resolve(commentsContent)
           }
-          resolve(commentsContent)
-        }
-      }
-    } else {
-      // if there is no comments, tell it
-      cleanedArticleContent += `<div><p><b>This article has no comments yet</b></p></div>`
-      const commentsContent = {
-        nComments: nComments,
-        content: cleanedArticleContent
-      }
-      resolve(commentsContent)
-    }
+        })
+      })
     })
   })
 }
